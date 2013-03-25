50151452f7330ed2c80da7822b5ca8d0a457d618
Tony Zhou <zhongwenzhou.wolf@gmail.com>
2013-03-25 11:45:12 +0800

    project init!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howl::Application.config.secret_token = 'ed32b977f54066b29e63334d6ec2f643d7160e602d74bb9311fbcd4fbe1217fe1a31d88ea3a3c259a660cbc96e7039471b110d875d789f2107700488c3bdba85'
