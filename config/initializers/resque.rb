c2c75487e9b0526ea6ab476e50d6cedee85ac9c3
Tony Zhou <zhongwenzhou.wolf@gmail.com>
2013-03-26 17:43:42 +0800

    add resque_schedule to project

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,6 @@
 # require 'resque_scheduler'
 # require 'resque_scheduler/tasks'
+require 'resque_scheduler'
 Resque.redis = 'localhost:6379/howl-resque'
+
+Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
